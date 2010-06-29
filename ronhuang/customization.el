11b08cbcf001fe950cece5a2efb7401189398139
Ron Huang <ronhuang@gmail.com>
2010-06-29 11:37:38 +0800

    Enabled YAML mode.

--- ronhuang/customization.el
+++ ronhuang/customization.el
@@ -97,6 +97,10 @@
 ;; iresize
 (require 'iresize)
 
+;; yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
 ;; reloads the current file
 (defun reload-file ()
   "Reloads the current file."
